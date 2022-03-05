26a644169156ded6917a0d138dffe778e5558b05
Canh Nguyen <nguyencanh127@gmail.com>
2022-03-05 15:40:14 +0700

    Make footer

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -2,12 +2,18 @@ import React, { useState, useEffect } from "react";
 
 import { Row, Col, Divider, Card } from "antd";
 
-import { ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
+import {
+  FacebookFilled,
+  InstagramFilled,
+  LinkedinFilled,
+  GithubFilled,
+  TwitterCircleFilled,
+} from "@ant-design/icons";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <Row align="middle" justify="center" className="products-container">
+      <Row justify="center" className="products-container">
         <Col
           className="gutter-row"
           lg={{ span: 5 }}
@@ -20,9 +26,13 @@ const Footer = () => {
             bordered={false}
             style={{ width: "100%" }}
           >
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <p className="footer-content">Support Center</p>
+            <p className="footer-content">Meta Gaming Blog</p>
+            <p className="footer-content">Shopping Guidelines</p>
+            <p className="footer-content">Payment</p>
+            <p className="footer-content">Delivery</p>
+            <p className="footer-content">Customer Services</p>
+            <p className="footer-content">Warranty Policy</p>
           </Card>
         </Col>
         <Col
@@ -37,9 +47,12 @@ const Footer = () => {
             bordered={false}
             style={{ width: "100%" }}
           >
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <p className="footer-content">What Is Meta Gaming</p>
+            <p className="footer-content">Recruitment</p>
+            <p className="footer-content">Terms And Services</p>
+            <p className="footer-content">Privacy Policy</p>
+            <p className="footer-content">Flash Sales</p>
+            <p className="footer-content">FAQs</p>
           </Card>
         </Col>
         <Col
@@ -54,9 +67,26 @@ const Footer = () => {
             bordered={false}
             style={{ width: "100%" }}
           >
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <p className="footer-content">
+              <img src="/images/paypal.png" alt="paypal" className="image" />
+              Pay With Paypal
+            </p>
+            <p className="footer-content">
+              <img src="/images/master.png" alt="master" className="image" />
+              Pay With MasterCard
+            </p>
+            <p className="footer-content">
+              <img
+                src="/images/delivery.jpg"
+                alt="delivery"
+                className="image"
+              />
+              Pay On Delivery
+            </p>
+            <p className="footer-content">
+              <img src="/images/free.jpg" alt="free" className="image" />
+              Free Ship
+            </p>
           </Card>
         </Col>
         <Col
@@ -67,12 +97,37 @@ const Footer = () => {
           xs={{ span: 22 }}
         >
           <Card title="Contact us" bordered={false} style={{ width: "100%" }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <p className="footer-content">
+              <FacebookFilled className="icon" style={{ color: "#1890ff" }} />
+              Facebook
+            </p>
+            <p className="footer-content">
+              <InstagramFilled className="icon" /> Instagram
+            </p>
+            <p className="footer-content">
+              <TwitterCircleFilled
+                className="icon"
+                style={{ color: "#1890ff" }}
+              />{" "}
+              Twitter
+            </p>
+
+            <p className="footer-content">
+              <LinkedinFilled className="icon" style={{ color: "#1890ff" }} />{" "}
+              <a href="https://www.linkedin.com/in/canh-nguyen-a47637159/">
+                Linkedin
+              </a>
+            </p>
+            <p className="footer-content">
+              <GithubFilled className="icon" style={{ color: "black" }} />{" "}
+              <a href="https://github.com/Canh-Nguyen-Kevin">Github</a>
+            </p>
           </Card>
         </Col>
       </Row>
+      <h4 style={{ width: "90%", margin: "0 auto", paddingBottom: "30px" }}>
+        &copy; 2022 Canh Nguyen all rights reserved
+      </h4>
     </div>
   );
 };
